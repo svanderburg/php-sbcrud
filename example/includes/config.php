72ff5e7274a47befb3b52578686173bc56a899b5
Sander van der Burg <svanderburg@gmail.com>
2022-02-22 23:28:34 +0100

    Migrate to Doxygen

--- example/includes/config.php
+++ example/includes/config.php
@@ -1,6 +1,7 @@
 <?php
 $config = array(
-	"dbDsn" => "mysql:host=localhost;dbname=books",
+	//"dbDsn" => "mysql:host=localhost;dbname=books",
+	"dbDsn" => "mysql:unix_socket=/home/sander/var/run/mysqld/mysqld.sock;dbname=books",
 	"dbUsername" => "root",
 	"dbPassword" => "admin"
 );
