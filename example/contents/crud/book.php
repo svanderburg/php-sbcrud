bedb543ffc4add06ba2821b18ac24fbc1cef1b62
Sander van der Burg <svanderburg@gmail.com>
2022-10-29 18:32:11 +0200

    Use http_build_query to construct page parameter

--- example/contents/crud/book.php
+++ example/contents/crud/book.php
@@ -3,7 +3,7 @@ $booksURL = $_SERVER["SCRIPT_NAME"]."/books";
 global $crudModel;
 ?>
 <p>
-	<a href="<?php print($booksURL); ?>?page=<?php print($crudModel->requestParameterMap->values["page"]->value); ?>">&laquo; Books</a> |
+	<a href="<?php print($booksURL); ?>?<?php print(http_build_query(array("page"=> $crudModel->requestParameterMap->values["page"]->value), "", "&amp;", PHP_QUERY_RFC3986)); ?>">&laquo; Books</a> |
 	<a href="<?php print($booksURL); ?>?__operation=create_book">Add book</a>
 </p>
 <?php
