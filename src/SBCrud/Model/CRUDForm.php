efde04470053d4f197fba32dfce8d0a98552482d
Sander van der Burg <svanderburg@gmail.com>
2023-03-23 20:45:46 +0100

    Make labels configurable

--- src/SBCrud/Model/CRUDForm.php
+++ src/SBCrud/Model/CRUDForm.php
@@ -17,10 +17,13 @@ class CRUDForm extends Form
 	 * @param $fields An associative array mapping field names to fields that should be checked and displayed
 	 * @param $operationParam Name of parameter that stores the operation to execute
 	 * @param $actionURL Action URL where the user gets redirected to (defaults to same page)
+	 * @param $submitLabel Label to be displayed on the submit button
+	 * @param $validationErrorMessage Error message displayed on form level when a field is invalid
+	 * @param $fieldErrorMessage Error message displayed for an invalid field
 	 */
-	public function __construct(array $fields, string $operationParam = "__operation", string $actionURL = null)
+	public function __construct(array $fields, string $operationParam = "__operation", string $actionURL = null, string $submitLabel = "Submit", string $validationErrorMessage = "One or more fields are invalid and marked with a red color", string $fieldErrorMessage = "This value is incorrect!")
 	{
-		parent::__construct($fields, $actionURL);
+		parent::__construct($fields, $actionURL, $submitLabel, $validationErrorMessage, $fieldErrorMessage);
 		$this->operationParam = $operationParam;
 
 		// Add a hidden field for the operation parameter
